1500df7954a3e888a0db2126b0521871a039e451
dmddjd <dmstjd99510@naver.com>
2025-06-30 17:21:26 +0900

    스껄!

--- src/main/java/com/kh/practice/controller/SelectAllMemberServlet.java
+++ src/main/java/com/kh/practice/controller/SelectAllMemberServlet.java
@@ -1,11 +1,17 @@
 package com.kh.practice.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.kh.practice.model.service.MemberServiceImpl;
+import com.kh.practice.model.vo.Member;
 
 /**
  * Servlet implementation class SelectAllMemberServlet
@@ -13,29 +19,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/member/selectAll")
 public class SelectAllMemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+ 
     public SelectAllMemberServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		List<Member> list = new MemberServiceImpl().selectAllMember();
+		
+		response.setContentType("application/json; charset=utf-8");
+		Gson gson = new Gson();
+		gson.toJson(list, response.getWriter());
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }

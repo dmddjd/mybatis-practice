6842879c7cca5e3cc72894f1abfe7e0ef8491b38
dmddjd <dmstjd99510@naver.com>
2025-07-01 15:49:57 +0900

    ㅗㅗ

--- src/main/java/com/kh/practice/controller/SearchMemberByIdServlet.java
+++ src/main/java/com/kh/practice/controller/SearchMemberByIdServlet.java
@@ -1,41 +1,35 @@
 package com.kh.practice.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class SearchMemberByIdServlet
- */
+import com.google.gson.Gson;
+import com.kh.practice.model.service.MemberService;
+import com.kh.practice.model.service.MemberServiceImpl;
+import com.kh.practice.model.vo.Member;
+
 @WebServlet("/member/searchById")
 public class SearchMemberByIdServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public SearchMemberByIdServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String userId = request.getParameter("userId");
+		
+		MemberService service = new MemberServiceImpl();
+		Member member = service.selectMemberById(userId);
+		
+		new Gson().toJson(member, response.getWriter());
+		response.setContentType("application/json; charset=uft-8");
+		
+		
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
